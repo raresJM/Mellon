c2fc74f0dfe40629533f0ddf77f6641ed168a4a0
raresJM <rares.scarlat.jm@gmail.com>
2017-02-16 23:40:14 +0200

    created the project Mellon

---
+++ Mellon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mellon
+{
+    [TestClass]
+    public class Mellon
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
