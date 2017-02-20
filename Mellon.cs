38873500669e10e25452ab18cb792acbf8d2d09f
raresJM <rares.scarlat.jm@gmail.com>
2017-02-20 23:13:37 +0200

    added test for 4 kilos, refactored some tests for the correct algorithm, added some new tests

--- Mellon.cs
+++ Mellon.cs
@@ -11,6 +11,8 @@ namespace Mellon
         {
             Assert.IsTrue(CheckIfEven(2));
         }
+
+
         public bool CheckIfEven(int number) 
         {
             bool result = false;
@@ -28,29 +30,45 @@ namespace Mellon
             Assert.IsTrue(CheckIfCanBeSplitted(8));
         }
         [TestMethod]
-        public void TestIfMellonCanBeSplitted_BadScenario_01()
+        public void TestIfMellonCanBeSplitted_GoodScenario_01()
         {
-            Assert.IsFalse(CheckIfCanBeSplitted(10));
+            Assert.IsTrue(CheckIfCanBeSplitted(10));
         }
         [TestMethod]
         public void TestIfMellonCanBeSplitted_BadScenario_02()
         {
             Assert.IsFalse(CheckIfCanBeSplitted(9));
         }
+
+        [TestMethod]
+        public void TestIfMellonCanBeSplitted_BadScenario_03()
+        {
+            Assert.IsFalse(CheckIfCanBeSplitted(2));
+        }
+        [TestMethod]
+        public void TestIfMellonCanBeSplitted_BadScenario_04()
+        {
+            Assert.IsFalse(CheckIfCanBeSplitted(3));
+        }
         [TestMethod]
         public void TestIfMellonCanBeSplitted_SixKilos()
         {
             Assert.IsTrue(CheckIfCanBeSplitted(6));
         }
+        [TestMethod]
+        public void TestIfMellonCanBeSplitted_FourKilos()
+        {
+            Assert.IsTrue(CheckIfCanBeSplitted(4));
+        }
 
         //The mellon can be splitted in two even number of kilos if 
-        //kilos is an even number and also kilos/2 is an even number!
+        //kilos is an even number and also kilos is larger than or equal to 4!
         public bool CheckIfCanBeSplitted(int kilos)
         {
             bool result = false;
             if (CheckIfEven(kilos)) 
             {
-                if (CheckIfEven(kilos / 2)) 
+                if (kilos >= 4) 
                 {
                     result = true;
                 }
